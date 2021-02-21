50d94583dbf09145238f1e044f63b8374a012f78
anish-pratheepkumar <anish.pratheepkumar@gmail.com>
2021-02-21 14:14:12 +0100

    modified to debug unicode error

--- packages/neural_network_model/neural_network_model/processing/preprocessors.py
+++ packages/neural_network_model/neural_network_model/processing/preprocessors.py
@@ -22,7 +22,8 @@ class TargetEncoder(BaseEstimator, TransformerMixin):
 
 
 def _im_resize(df, n, image_size):
-    im = cv2.imread(r"df[n]")
+    print(df[n])
+    im = cv2.imread(df[n])
     im = cv2.resize(im, (image_size, image_size))
     return im
 
